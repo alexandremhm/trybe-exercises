1888092dfa7559248976097cfc4a94b03d5c2611
Matheus_Alexandre <alexandremhm@gmail.com>
2021-03-24 19:13:40 -0300

    Adiciona o exercicio 9

--- exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao_1/even_odd.js
+++ exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao_1/even_odd.js
@@ -1,8 +1,8 @@
-num1 = 3;
-num2 = 3;
-num3 = 3;
+num1 = 2;
+num2 = 2;
+num3 = 2;
 
-if (num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0) {
+if (num1 % 2 != 0 || num2 % 2 != 0 || num3 % 2 != 0) {
     console.log(true);
 } else {
     console.log(false);
