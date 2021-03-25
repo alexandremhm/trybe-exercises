fb22c52714324a3116ca24fcb2f3b64032af8e20
Matheus_Alexandre <alexandremhm@gmail.com>
2021-03-25 13:48:17 -0300

    Adiciona terceira  parte dos exercicios de conteudo

--- exercises/bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicios_conteudo.js
+++ exercises/bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicios_conteudo.js
@@ -26,3 +26,11 @@ let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
 for (let index = 0; index < groceryList.length; index += 1) {
     console.log(groceryList[index]);
 }
+
+//Exercicio 5
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (let cadaNome of names) {
+    console.log(cadaNome);
+}
+
