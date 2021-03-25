d7bd9676847600c3d851a58c60b04ef5a85bcfbc
Matheus_Alexandre <alexandremhm@gmail.com>
2021-03-25 13:57:57 -0300

    Adiciona exercicio 2

--- exercises/bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicios_dia.js
+++ exercises/bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicios_dia.js
@@ -4,3 +4,12 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (let index = 0; index < numbers.length; index += 1){
     console.log(numbers[index]);
 }
+
+//Exercicio 2
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+for (let index = 0; index < numbers.length; index += 1){
+    sum = sum + numbers[index];
+}
+console.log(sum);
