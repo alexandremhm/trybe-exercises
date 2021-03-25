ae2e62af3862166ecf5fa7c46c8629430d771034
Matheus_Alexandre <alexandremhm@gmail.com>
2021-03-25 15:20:32 -0300

    Adiciona exercicio 7

--- exercises/bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicios_dia.js
+++ exercises/bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicios_dia.js
@@ -62,3 +62,13 @@ for (index = 0; index < numbers.length; index += 1){
 } else {
     console.log(oddNumbers);
 }
+
+//Exercicio 7
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let smallerNum = numbers[0];
+for (index = 0; index < numbers.length; index += 1){
+    if (numbers[index] < smallerNum) {
+        smallerNum = numbers[index];
+    }
+} console.log(smallerNum);
