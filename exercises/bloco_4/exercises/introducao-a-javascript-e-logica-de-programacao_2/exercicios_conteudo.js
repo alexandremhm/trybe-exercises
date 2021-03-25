8cb61c8a9f859c3e4ff54a645ddac66b0e64c6d7
Matheus_Alexandre <alexandremhm@gmail.com>
2021-03-25 13:37:25 -0300

    Adiciona segunda  parte dos exercicios de conteudo

--- exercises/bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicios_conteudo.js
+++ exercises/bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicios_conteudo.js
@@ -18,3 +18,11 @@ let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 menu.push('Contato');
 
 console.log(menu);
+
+//Exercicio 4
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let index = 0; index < groceryList.length; index += 1) {
+    console.log(groceryList[index]);
+}
