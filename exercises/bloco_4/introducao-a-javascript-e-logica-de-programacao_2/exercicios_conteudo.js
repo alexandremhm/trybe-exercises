c08cbda07c2f3ef990b594b64eeedab6d1d81bb9
Matheus_Alexandre <alexandremhm@gmail.com>
2021-03-26 11:26:38 -0300

    Reorganiza diretorios

---
+++ exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao_2/exercicios_conteudo.js
@@ -0,0 +1,36 @@
+//Exercicio 1
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+//Exercicio 2
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+
+//Exercicio 3
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push('Contato');
+
+console.log(menu);
+
+//Exercicio 4
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let index = 0; index < groceryList.length; index += 1) {
+    console.log(groceryList[index]);
+}
+
+//Exercicio 5
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (let cadaNome of names) {
+    console.log(cadaNome);
+}
+
---
+++ exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao_2/exercicios_dia.js
@@ -0,0 +1,144 @@
+//Exercicio 1
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+for (let index = 0; index < numbers.length; index += 1){
+    console.log(numbers[index]);
+}
+
+//Exercicio 2
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+for (let index = 0; index < numbers.length; index += 1){
+    sum = sum + numbers[index];
+}
+console.log(sum);
+
+//Exercicio 3
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+for (let index = 0; index < numbers.length; index += 1){
+    sum = sum + numbers[index];
+}
+let average = sum / numbers.length;
+console.log(average);
+
+//Exercicio 4
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+for (let index = 0; index < numbers.length; index += 1){
+    sum = sum + numbers[index];
+}
+let average = sum / numbers.length;
+if (average > 20){
+    console.log('Valor maior que 20!');
+} else {
+    console.log('Valor menor que 20');
+}
+
+//Exercicio 5
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let higherNum = 0;
+for (index = 0; index < numbers.length; index += 1){
+    if (numbers[index] > higherNum) {
+        higherNum = numbers[index];
+    }
+} console.log(higherNum);
+
+//Exercicio 6
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let oddNumbers = 0;
+for (index = 0; index < numbers.length; index += 1){
+    if(numbers[index] % 2 != 0){
+        oddNumbers += 1;
+    }
+    
+} if(oddNumbers === 0) {
+    console.log('Nenhum valor ímpar encontrado');
+} else {
+    console.log(oddNumbers);
+}
+
+//Exercicio 7
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let smallerNum = numbers[0];
+for (index = 0; index < numbers.length; index += 1){
+    if (numbers[index] < smallerNum) {
+        smallerNum = numbers[index];
+    }
+} console.log(smallerNum);
+
+//Exercicio 8
+
+let arrFor = [];
+for (index = 0; index <= 25; index += 1){
+    arrFor.push(index);
+}
+console.log(arrFor);
+
+//Exercicio 9
+
+let arrFor = [];
+for (index = 0; index <= 25; index += 1){
+    arrFor.push(index);
+}
+for (number = 0; number < arrFor.length; number += 1) {
+    console.log (number / 2);
+}
+
+// Exercicio 9 outra maneira
+
+let arrFor = [];
+for (index = 0; index <= 25; index += 1){
+    arrFor.push(index/2);
+}
+console.log(arrFor);
+
+
+//Exercicio bonus 1
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+for (let index = 1; index < numbers.length; index += 1){
+    for (let indexDois = 0; indexDois < index; indexDois += 1) {
+        if  (numbers[index] < numbers[indexDois]) {
+            let position = numbers[index];
+            numbers[index] = numbers[indexDois];
+            numbers[indexDois] = position;                        
+        }
+    }
+} 
+console.log(numbers);
+
+//Exercicio bonus 2
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+for (let index = 1; index < numbers.length; index += 1){
+    for (let indexDois = 0; indexDois < index; indexDois += 1) {
+        if  (numbers[index] > numbers[indexDois]) {
+            let position = numbers[index];
+            numbers[index] = numbers[indexDois];
+            numbers[indexDois] = position;                        
+        }
+    }
+} 
+console.log(numbers);
+
+//Exercicio bonus 3
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let newArray = [];
+
+for (index = 0; index < numbers.length; index += 1) {    
+        if (numbers[index + 1]) {
+            newArray.push(numbers[index] * numbers[index +1]);
+                    } else {
+            newArray.push(numbers[index] * 2);
+        }
+}
+
+console.log(newArray);
