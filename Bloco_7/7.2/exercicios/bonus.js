3684f9db0d394c420e5ced208ec0a953c80eaf8c
Matheus_Alexandre <alexandremhm@gmail.com>
2021-04-27 20:40:23 -0300

    Add bonus 2

--- Bloco_7/7.2/exercicios/bonus.js
+++ Bloco_7/7.2/exercicios/bonus.js
@@ -36,6 +36,31 @@ const getMathStudents = (object, subject) => {
 
 console.log(getMathStudents(allLessons, 'História'));
 
+console.log('---------------------------------------------------->')
+
 
 // Crie uma função que deverá retornar um objeto que representa o relatório do professor ou professora, as aulas que ele ou ela ministrou e o número total de estudantes. Use o objeto criado no exercício 5:
 
+const getTeacherSubjects = (object, teacher) => {
+  let sum = [];
+  for (const element in object) {
+    for (const element2 in object) {
+      if (object[element2].professor === teacher) {
+        sum.push(object[element2].materia);
+      }
+    } return sum
+  }
+}
+
+console.log(getTeacherSubjects(allLessons, 'Maria Clara'));
+
+const getSubjectAbstract = (object, teacher, subject) => {
+  let abstract = {
+    professor: teacher,
+    aulas: getTeacherSubjects(object, teacher),
+    estudantes: getMathStudents(object, subject),
+  }
+  return abstract
+}
+
+console.log(getSubjectAbstract(allLessons, 'Maria Clara', 'Matemática'))
