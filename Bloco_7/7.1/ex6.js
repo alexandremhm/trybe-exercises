6614b264aa0a4b4832afacc4241ff6d562a4ed8c
Matheus_Alexandre <alexandremhm@gmail.com>
2021-04-27 17:58:39 -0300

    Adiciona ex conteudo 7.2

--- Bloco_7/7.1/ex6.js
+++ Bloco_7/7.1/ex6.js
@@ -8,3 +8,24 @@ const skillsString = (array) => `${replaceX("Tryber x aqui!", 'Matheus')}
 Minhas cinco principais habilidades são: ${array.sort().join()}`
 
 console.log(skillsString(['HTML', 'CSS', 'JS', 'GIT', 'GITHUB']));
+
+const customer1 = {
+  firstName: 'Roberto',
+  age: 22,
+  job: 'Teacher',
+};
+
+console.log(customer1);
+
+customer1.lastName = 'Faria';
+console.log(customer1);
+
+const customer2 = {
+  firstName: 'Maria',
+  age: 23,
+  job: 'Medic',
+};
+
+console.log(customer2);
+customer2['lastName'] = 'Silva';
+console.log(customer2);
