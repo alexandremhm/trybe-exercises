8e04414e654ecde61c190d55fc9ab3d13ca7c400
Matheus_Alexandre <alexandremhm@gmail.com>
2021-04-27 20:26:25 -0300

    Refatora ex bonus 1

--- Bloco_7/7.2/exercicios/bonus.js
+++ Bloco_7/7.2/exercicios/bonus.js
@@ -23,18 +23,18 @@ console.log(allLessons);
 
 // Crie uma função para contar quantos estudantes assistiram às aulas de Matemática. Use o objeto criado no exercício 5.
 
-const getMathStudents = (object) => {
+const getMathStudents = (object, subject) => {
   let sum = 0;
   for (const element in object) {
     for (const element2 in object) {
-      if (object[element2].materia === 'Matemática') {
+      if (object[element2].materia === subject) {
         sum = sum + object[element2].numeroEstudantes;
       }
     } return sum
   }
 }
 
-console.log(getMathStudents(allLessons));
+console.log(getMathStudents(allLessons, 'História'));
 
 
 // Crie uma função que deverá retornar um objeto que representa o relatório do professor ou professora, as aulas que ele ou ela ministrou e o número total de estudantes. Use o objeto criado no exercício 5:
