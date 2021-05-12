2ee0ee4d81824fc4334d9e240c2fecae84073023
Matheus_Alexandre <alexandremhm@gmail.com>
2021-05-12 19:04:04 -0300

    Add exs dia

---
+++ exercises/Bloco_8/Bloco_9/9.2/exdia2.js
@@ -0,0 +1,33 @@
+const generateRandowNumber = () => Math.pow((Math.round((Math.random() * 50))), 2);
+
+const promiseNumber = () => {
+
+  return new Promise((resolve, reject) => {
+
+  const randonNumber = generateRandowNumber();
+
+  const newArr = [randonNumber, randonNumber, randonNumber, randonNumber, randonNumber, randonNumber, randonNumber, randonNumber, randonNumber, randonNumber];
+
+  const sum = newArr.reduce((acc, number) => acc += number, 0);
+
+  if (sum < 8000) {
+    return resolve(sum)
+  }
+   return reject(); 
+
+  })  
+
+}
+
+const promiseNumberResult = () => {
+  promiseNumber()
+    .then((number) => {
+      let newArr = [number/2, number/3, number/5, number/10]
+      return newArr;
+    })
+    .then((number) => number.reduce((acc, number) => acc += number, 0))
+    .then((number) => console.log(number))
+    .catch(() => console.log('É mais de oito mil! Essa promise deve estar quebrada!'))
+}
+
+promiseNumberResult();
