6613c2638f81f2dec3495a1c2d4532780257d715
Matheus_Alexandre <alexandremhm@gmail.com>
2021-05-18 16:09:50 -0300

    add ex 2

--- exercises/Bloco_10/jest_ex/sum.js
+++ exercises/Bloco_10/jest_ex/sum.js
@@ -1,5 +1,3 @@
-
-
 const sum = (a, b) => {
   if (typeof a !== 'number' || typeof b !== 'number') {
     throw new Error('parameters must be numbers');
--- exercises/Bloco_10/jest_ex/sum.test.js
+++ exercises/Bloco_10/jest_ex/sum.test.js
@@ -1,5 +1,19 @@
-const sumN = (a, b) => a + b;
+const sum = require('./sum');
 
-test('sums two values', () => {
-  expect(sumN(2, 3)).toEqual(5);
+describe('Exercicio 1 - sum function', () => {
+  test('sum(4, 5) retorna 9', () => {
+    expect(sum(4, 9)).toBe(9);
+  });
+
+  test('sum(0, 0) retorna 0', () => {
+    expect(sum(0, 0)).toBe(0);
+  });
+
+  test(`sum(4, '5') testa se dispara erro`, () => {
+    expect(() => {sum(4, '5').toThrow()});
+  });
+
+  test(`sum(4, '5') retorna erro: parameters must be numbers`, () => {
+    expect(() => {sum(4, '5').toThrow(new Error('parameters must be numbers'))});
+  });
 });
