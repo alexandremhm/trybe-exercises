ac3258e89e1e7158fadfb2aba17b987ae3a03e07
Matheus_Alexandre <alexandremhm@gmail.com>
2021-05-19 18:36:27 -0300

    Add ex 1 e 2

---
+++ exercises/Bloco_10/10.1/ex1.test.js
@@ -0,0 +1,12 @@
+// Escreva um teste que verifique a chamada do callback de uma função uppercase , que transforma as letras de uma palavra em letras maiúsculas. Lembre-se de ter cuidado com os falso-positivos em testes assíncronos.
+
+const uppercase = (str, callback) => {
+  callback(str.toUpperCase());
+};
+
+test('testa a função uppercase', (done) => {
+  uppercase('bola', (callback) => {
+    expect(callback).toBe('BOLA')
+    done();
+  })
+})
