530ad863880c41b31d97988d3ffcd38dd1ed6190
Matheus_Alexandre <alexandremhm@gmail.com>
2021-05-24 09:39:17 -0300

    Add mudancas

--- exercises/Bloco_10/10.3/ex6.js
+++ exercises/Bloco_10/10.3/ex6.js
@@ -1,11 +1,17 @@
 // Crie uma função que faça requisição para a api dog pictures . Mocke a requisição e crie dois testes. O primeiro deve interpretar que a requisição se resolveu e teve como valor "request sucess". O segundo deve interpretar que a requisição falhou e ter como valor "request failed". Crie todos os testes que achar necessário.
+const fetch = require('node-fetch')
 
 const url = 'https://dog.ceo/api/breeds/image/random'
 
-function newFetch(url) {
+const headers = {
+  method: 'GET',
+  headers: { 'Accept': 'application/Json' }
+  };
+
+function newFetch(url, headers) {
   return new Promise((resolve, reject) => {
     if (url === 'https://dog.ceo/api/breeds/image/random') {
-      fetch(url)
+      fetch(url, headers)
         .then((r) => r.json())
         .then((r) => resolve(r));
     } else {
@@ -13,5 +19,7 @@ function newFetch(url) {
     }
   });
 }
+
+newFetch(url, headers)
 
 module.exports = { newFetch };
